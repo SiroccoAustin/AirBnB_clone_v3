4c2b0264c3906a4bc59e7734a7f9f0aefed0d9d9
Austin <Martinaustin319@gmail.com>
2024-02-05 03:28:03 -0800

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,8 @@
 """Create routes"""
 from api.v1.views import app_views
 from flask import jsonify
-@app.views.route("/status", methods=["GET"], strict_slashes=False)
+
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status():
     """Get status"""
     return jsonify({"status": "OK"})
